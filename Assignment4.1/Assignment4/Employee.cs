90a587a07d774715db1f0482602ff7765df83427
VaibhavGangwar-Code <vaibhavgangwar1998@gmail.com>
2020-11-02 15:33:02 +0530

    I have added the assignment

---
+++ Assignment4.1/Assignment4/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Employee
+    {
+        protected int Empid;
+        protected string Empname;
+        protected float Salaryperhour;
+
+        internal Employee(int Empid, string Empname, float Salaryperhour)
+        {
+            this.Empid = Empid;
+            this.Empname = Empname;
+            this.Salaryperhour = Salaryperhour;
+        }
+    }
+
+    class ParttimeEmployee : Employee
+    {
+        float hoursofworking;
+        internal ParttimeEmployee(int Empid, string Empname, float Salaryperhour, float hoursofworking) : base(Empid, Empname, Salaryperhour)
+        {
+            this.hoursofworking = hoursofworking;
+        }
+        internal float CalculateSalary()
+        {
+            return Salaryperhour * hoursofworking;
+        }
+        internal void DisplayDetails()
+        {
+            Console.WriteLine("Empolyeeid:{0}\nEmployee Name:{1}\nSalary:{2}", Empid, Empname, CalculateSalary());
+
+        }
+        static void Main()
+        {
+            ParttimeEmployee ptemployee = new ParttimeEmployee(10662951, "Vaibhav", 999.99f, 8.5f);
+            ptemployee.DisplayDetails();
+            Console.Read();
+
+        }
+
+    }
+
+}
+
+    
---
+++ Assignment4.1/Assignment4/Q3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    interface Icar
+    {
+        string Break();
+        string Engine();
+
+    }
+
+    class Benz : Icar
+    {
+        public string Break()
+        {
+            return "auto break";
+        }
+
+        public string Engine()
+        {
+            return "OM654 engine";
+
+        }
+
+        internal void BenzFunctionality()
+        {
+            Console.WriteLine("Benz car has Pre-Safe Break and Distronic Plus Technology");
+        }
+    }
+
+    class Toyota : Icar
+    {
+        public string Break()
+        {
+            return "pad disk break";
+        }
+
+        public string Engine()
+        {
+            return "dynamic force engine";
+        }
+
+        internal void ToyotaFunctionality()
+        {
+            Console.WriteLine("Toyota cars have electronic modulated suspension");
+        }
+    }
+
+
+    class Cars
+    {
+        static void Main()
+        {
+            Benz BENZ = new Benz();
+            Console.WriteLine("Benz break : {0} and Benz Engine:{1}", BENZ.Break(), BENZ.Engine());
+            BENZ.BenzFunctionality();
+
+            Toyota TOYOTA = new Toyota();
+            Console.WriteLine("\nToyota break : {0} and Toyota Engine:{1}", TOYOTA.Break(), TOYOTA.Engine());
+            TOYOTA.ToyotaFunctionality();
+
+            Console.Read();
+        }
+    }
+
+}
+
+
+
